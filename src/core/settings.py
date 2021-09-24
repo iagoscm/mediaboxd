a4ef0d2c8072523dc8d39fa48b34e497d8af87a9
Icaro Pereira <icarooliv2@gmail.com>
2021-09-23 22:37:30 -0300

    Adiciona configuraçao de mensagens do Django
    Co-authored-by: Luan Melo <luanmelo.q@gmail.com>
    Co-authored-by: William Almeida <will.allmeida@gmail.com>
    Co-authored-by: Iago Matos <iagomatosousa@gmail.com>
    Co-authored-by: Hellen Faria <202016480@aluno.unb.br>
    Co-authored-by: Gabriel Vitor <gv6609588@gmail.com>
    Co-authored-by: Henrique Pucci <202016506@aluno.unb.br>

--- src/core/settings.py
+++ src/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -69,6 +70,14 @@ TEMPLATES = [
         },
     },
 ]
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
