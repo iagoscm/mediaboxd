9d7907eebac07152b3ca9989cc4fac4bfe2299dc
Icaro Pereira <icarooliv2@gmail.com>
2021-09-23 22:35:30 -0300

    Adiciona data de criação e atualização na classe Review
    Co-authored-by: Luan Melo <luanmelo.q@gmail.com>
    Co-authored-by: William Almeida <will.allmeida@gmail.com>
    Co-authored-by: Iago Matos <iagomatosousa@gmail.com>
    Co-authored-by: Hellen Faria <202016480@aluno.unb.br>
    Co-authored-by: Gabriel Vitor <gv6609588@gmail.com>
    Co-authored-by: Henrique Pucci <202016506@aluno.unb.br>

--- src/reviews/models.py
+++ src/reviews/models.py
@@ -7,8 +7,10 @@ class Review(models.Model):
      """
      title = models.CharField(max_length=200)
      content = models.TextField()
-     date_published = models.DateTimeField(auto_now_add=True)
+     published_at = models.DateTimeField(auto_now_add=True)
      author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
 
      def __str__(self):
          """
