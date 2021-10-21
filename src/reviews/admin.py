0933337037c4560898645717e19a46635e686754
Hellen159 <202016480@aluno.unb.br>
2021-10-20 21:21:48 -0300

    adiciona funcionalidade tags
    Co-Authored-By: gabrielvaraujo <88348409+gabrielvaraujo@users.noreply.github.com>

--- src/reviews/admin.py
+++ src/reviews/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Review
+from .models import Review , Tag
 
 admin.site.register(Review)
+admin.site.register(Tag)
--- src/reviews/forms.py
+++ src/reviews/forms.py
@@ -1,12 +1,18 @@
 from django import forms
-from .models import Review
+from .models import Review, Tag
 
 
 class ReviewForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField( 
+        queryset=Tag.objects.all(), 
+        widget=forms.CheckboxSelectMultiple, 
+        required=False)
+
     class Meta:
         model = Review
-        fields = ['title', 'content']
+        fields = ['title', 'content','tags']
         labels = {
             'title': 'Título',
+            'tags': 'Tags',
             'content': 'Conteúdo',
         }
