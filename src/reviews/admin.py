9945a9992720d09bbf41eca2c71f81e7deb22dc5
Icaro Pereira <icarooliv2@gmail.com>
2021-10-23 18:00:22 -0300

    mergeback 'tags' branch

--- src/reviews/admin.py
+++ src/reviews/admin.py
@@ -1,4 +1,6 @@
+
 from django.contrib import admin
-from .models import Review
+from .models import Review , Tag
 
 admin.site.register(Review)
+admin.site.register(Tag)
--- src/reviews/models.py
+++ src/reviews/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Tag(models.Model):
+    title = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
 class Review(models.Model):
      """
@@ -11,6 +17,7 @@ class Review(models.Model):
      author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
      created_at = models.DateTimeField(auto_now_add=True)
      updated_at = models.DateTimeField(auto_now=True)
+     tags = models.ManyToManyField(Tag)
 
      def __str__(self):
          """
