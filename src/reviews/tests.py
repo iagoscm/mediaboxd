c3febcbd73c3f909b6a26d1896db137470aded1b
Icaro Pereira <icarooliv2@gmail.com>
2021-10-26 21:11:13 -0300

    adiciona teste escrito no dojo
    Co-authored-by: Luan Melo <luanmelo.q@gmail.com>
    Co-authored-by: William Almeida <will.allmeida@gmail.com>
    Co-authored-by: Iago Matos <iagomatosousa@gmail.com>
    Co-authored-by: Hellen Faria <202016480@aluno.unb.br>
    Co-authored-by: Gabriel Vitor <gv6609588@gmail.com>
    Co-authored-by: Henrique Pucci <202016506@aluno.unb.br>

--- src/reviews/tests.py
+++ src/reviews/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Review, Media
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class ReviewTestCase(TestCase):
+    def setUp(self):
+        self.author = User.objects.create(username='testuser', password='12345')
+        self.media = Media.objects.create(
+            cover_url="https://i.imgur.com/Zvb1d0t.jpeg",
+            name="The Matrix",
+            description="A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+            release_date="1999-03-31",
+            media_type="movie",
+        )
+
+    def test_reviews_can_created(self):
+        title = 'Test Review'
+        content = 'This is a test review'
+        review = Review.objects.create(
+            title=title,
+            content=content,
+            author=self.author,
+            media=self.media,
+        )
+
+        review_encontrada = Review.objects.get(id=review.id)
+        self.assertEqual(review_encontrada.title, title)
